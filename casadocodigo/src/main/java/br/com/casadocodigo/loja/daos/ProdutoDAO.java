248f8a48c4fd396763c5717b54ff675554a61bfd
BRTOKEN\Altair <altair.lage@datablink.com>
2017-10-13 18:48:22 -0300

    Até o fim dos testes

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.daos;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,6 +10,7 @@ import javax.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Repository
 @Transactional
@@ -30,5 +32,13 @@ public class ProdutoDAO {
 				+ " join fetch p.precos preco where p.id = " + id, Produto.class);
 		Produto p = tp.getSingleResult();
 		return p;
+	}
+	
+	public BigDecimal somaProdutoPorTipoPreco(TipoPreco tipoPreco){
+		TypedQuery<BigDecimal> query = manager.createQuery("select sum(preco.valor) from Produto p"
+				+ " join p.precos preco where preco.tipo = :tipoPreco", BigDecimal.class);
+		query.setParameter("tipoPreco", tipoPreco);
+		
+		return query.getSingleResult();
 	}
 }
